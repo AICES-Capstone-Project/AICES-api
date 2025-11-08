f2908cf591545626d0405c17d15d9c31d32c6970
Hoàng Thành <hoangthanh5264@gmail.com>
2025-11-08 16:52:17 +0700

    fix deployment

--- API/Program.cs
+++ API/Program.cs
@@ -345,7 +345,7 @@ var app = builder.Build();
 // ------------------------
 // ?? SWAGGER
 // ------------------------
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
